02f34f7926eb4086b620fb9eb1f7832118b4b445
Fei Wang <silviafey.wang@utoronto.ca>
2025-07-27 02:45:28 +0000

    Updated configs.

---
+++ run.sh
@@ -0,0 +1,22 @@
+# llamafactory-cli train my_configs/mixtral_lora_sft_attn.yaml
+
+# llamafactory-cli train my_configs/mixtral_lora_sft_ffn.yaml
+
+# llamafactory-cli train my_configs/olmoe_lora_sft_all.yaml
+
+# llamafactory-cli train my_configs/olmoe_lora_sft_attn.yaml
+
+# llamafactory-cli train my_configs/olmoe_lora_sft_ffn.yaml
+
+# llamafactory-cli train my_configs/olmoe_lora_dpo_all.yaml
+
+# llamafactory-cli train my_configs/olmoe_lora_dpo_attn.yaml
+
+# llamafactory-cli train my_configs/olmoe_lora_dpo_ffn.yaml
+
+# llamafactory-cli train my_configs/mixtral_lora_dpo_all.yaml
+
+llamafactory-cli train my_configs/mixtral_lora_dpo_attn.yaml
+
+llamafactory-cli train my_configs/mixtral_lora_dpo_ffn.yaml
+
